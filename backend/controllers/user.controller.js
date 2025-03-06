1941bdb131993cdc0e7317090a1bf4108bfdc7ac
Divs-Git <divyansh.sri258@gmail.com>
2025-03-06 15:54:46 +0530

    added log out logic

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -110,3 +110,13 @@ export const login = async (req, res) => {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie('jwt');
+    res.status(200).json({ message: 'User logged out successfully' });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
